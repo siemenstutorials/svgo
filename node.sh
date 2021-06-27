a1cd95ad8cdffcd8bb1cb78e4b32e05ee85405c9
Siemens Tutorials <55917384+siemenstutorials@users.noreply.github.com>
2021-06-27 11:37:42 +0900

    Update node.sh

--- node.sh
+++ node.sh
@@ -154,6 +154,7 @@ function v2richo(){
     curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
     curl -L https://raw.githubusercontent.com/docker/compose/1.8.0/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
+    cp -rf /usr/local/bin/docker-compose /usr/bin/docker-compose
     clear
 #config
     sed -i "s|SMT|v${Dockerid}|" docker-compose.yml
